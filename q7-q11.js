02c45766eac970818037e2689e000b37f55b4a73
Ruben Zaldanan <rooben.zaldana@gmail.com>
2022-01-25 11:09:50 -0500

    finished assignment

--- q7-q11.js
+++ q7-q11.js
@@ -1,14 +1,24 @@
 const x = 6
 
-// 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" using without using arrow functions.
+// 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" without using arrow functions.
+
+function sum(num1, num2) {
+
+  return num1 + num2 + x;
+
+}
 
 // 8. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x", using arrow functions.
 
+const sum = (num1, num2) => num1 + num2 + x;
+
 // 9. Write a function that returns another function. (use arrow functions please)
 
+const funct1 = () => {
+  return () => 1 + 1;
+};
 
 // 10. given the following code:
-
 
 const getFunction = () => {
   const y = 5;
@@ -22,6 +32,7 @@ console.log(getFunction()(2))
 
 // Explain why the function that returns from getFunction still has access to "y" if "y" is not a global variable.
 
+// Y is inside the lexical environment of getFunction.
 
 
 // 11. write a function that takes "couldThrowError()" as a callback argument.
@@ -29,12 +40,25 @@ console.log(getFunction()(2))
 // Make sure to handle errors that could be thrown by "couldThrowError()"
 
 const couldThrowError = () => {
-  
-  if(Math.ceil(Math.random() * 2) < 2){
+
+  if (Math.ceil(Math.random() * 2) < 2) {
     throw new Error("Error was thrown");
   }
-  
+
   return 'success'
 }
 
+function problem11(couldThrowError) {
 
+  try {
+
+    couldThrowError();
+
+
+  } catch (error) {
+
+    console.log("Sorry there was an error");
+    console.log('error: ', error);
+
+  }
+}
